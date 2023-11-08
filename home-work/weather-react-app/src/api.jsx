3d03c53186a5515582bd01c02198110427182f85
rebekahho <adegboye.tosin.r@gmail.com>
2023-11-08 13:00:03 -0800

    adding my weather app assignment

---
+++ home-work/weather-react-app/src/api.jsx
@@ -0,0 +1,13 @@
+const url = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities";
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "ac5fc94d00mshf00dde64a8b87e8p1f62e0jsn562eef7f3f85",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "f40fd8feffda72b9a5d052c27ca9fb99";
