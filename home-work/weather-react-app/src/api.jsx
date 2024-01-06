b928921d89aa435369e46b6991dd6774b32d692f
Becky codes <104319043+BeckyyCodes@users.noreply.github.com>
2024-01-06 14:07:41 +0100

    Update api.jsx
    added the api keys for deployment to work

--- home-work/weather-react-app/src/api.jsx
+++ home-work/weather-react-app/src/api.jsx
@@ -2,7 +2,7 @@ const url = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities";
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "", //enter your rapid api key here
+    "X-RapidAPI-Key": "ac5fc94d00mshf00dde64a8b87e8p1f62e0jsn562eef7f3f85", //enter your rapid api key here
     //this is my key ac5fc94d00mshf00dde64a8b87e8p1f62e0jsn562eef7f3f85
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
@@ -11,5 +11,5 @@ export const geoApiOptions = {
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = ""; // enter your key from openweather API
+export const WEATHER_API_KEY = "f40fd8feffda72b9a5d052c27ca9fb99"; // enter your key from openweather API
 //this is my key f40fd8feffda72b9a5d052c27ca9fb99
